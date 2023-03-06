01e2b8476b7467be2c55a286467cc9fe4aac29ef
Lou K <84414167+Loustoc@users.noreply.github.com>
2023-03-06 22:03:10 +0100

    test display axis

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const acl = new Accelerometer({ frequency: 60 });
 acl.addEventListener("reading", () => {
   console.log(`Acceleration along the X-axis ${acl.x}`);
+  document.getElementById("x").textContent = acl.x;
   console.log(`Acceleration along the Y-axis ${acl.y}`);
+  document.getElementById("y").textContent = acl.y;
   console.log(`Acceleration along the Z-axis ${acl.z}`);
+  document.getElementById("z").textContent = acl.z;
 });
 acl.start();
