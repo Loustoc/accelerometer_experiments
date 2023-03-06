9fdcef2532df9606164ec6ce4567138f52f501b5
Lou K <84414167+Loustoc@users.noreply.github.com>
2023-03-06 22:11:19 +0100

    correct test

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ window.addEventListener(
   orientationChanged,
   true
 );
+function orientationChanged(event)
+{
+    // var beta = event.beta;
+    // var gamma = event.gamma;
 
-const acl = new Accelerometer({ frequency: 60 });
-acl.addEventListener("reading", () => {
-  console.log(`Acceleration along the X-axis ${acl.x}`);
-  document.getElementById("x").textContent = acl.x;
-  console.log(`Acceleration along the Y-axis ${acl.y}`);
-  document.getElementById("y").textContent = acl.y;
-  console.log(`Acceleration along the Z-axis ${acl.z}`);
-  document.getElementById("z").textContent = acl.z;
-});
-acl.start();
+    // if(beta > 90)
+    //     beta = 90;
+
+    // if(beta < -90)
+    //     beta = -90;
+
+    // beta += 90;
+    // gamma += 90;
+
+    // body.style.backgroundPosition = (x + gamma - add/2) + 'px ' + (y + beta - add/2) + 'px';
+    var absolute = event.absolute;
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var gamma = event.gamma;
+    document.getElementById("x").textContent = beta;
+    document.getElementById("y").textContent = gamma;
+    document.getElementById("z").textContent = alpha;
+}
+
